718c8ee75cdb43a80ccb9bb3ea784cc97c1c11b3
Rukshan <arkruka@gmail.com>
2022-09-08 14:25:54 +0530

    <type>[optional scope]: <description>
    [optional body]
    
    [optional footer(s)]

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/deposit-research', function() {
+    return view('depositResearch');
+});
+
 require __DIR__.'/auth.php';
