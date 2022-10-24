4fe5efcb7d374b27e0c14ce0f6737d9e2579df93
Rukshan <arkruka@gmail.com>
2022-10-25 00:30:37 +0530

    notification component initial commit

---
+++ resources/views/components/notification.blade.php
@@ -0,0 +1,21 @@
+@props(['type', 'message'])
+
+@php
+$success = false;
+$error = false;
+if ($type == 'success') {
+    $success = true;
+} elseif ($type == 'error') {
+    $error = true;
+}
+@endphp
+
+<span @class([
+    'p-4 block rounded-lg mb-4',
+    'font-bold' => $success,
+    'text-red-700' => $error,
+    'bg-red' => $error,
+    'bg-green-200' => $success,
+    'bg-red-200' => $error,
+    'text-green-700' => $success,
+])>{{ $message ?? $slot }}</span>
