41494beda66895e8a2968c427266d367c98a4138
Rukshan <arkruka@gmail.com>
2022-10-24 16:18:43 +0530

    Create and edit organizations

--- resources/views/organizationView.blade.php
+++ resources/views/organizationView.blade.php
@@ -9,49 +9,57 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
-          <form action="/organization" method="post">
+          <form action="/organization/{{ $organization->id }}" method="post">
+            @if ($mode == 'edit')
+              @method('put')
+            @endif
             @csrf
             <div class="control">
               <x-label for="name" :value="__('Name')" />
-              <x-input class="block mt-1 w-full" id="name" placeholder="Organization name" type="text"
-                name="name" required autofocus>
+              <x-input class="block mt-1 w-full" value="{{ $organization->name }}" id="orgName"
+                placeholder="Organization name" type="text" name="orgName" required autofocus>
               </x-input>
             </div>
             <div class="control mt-4">
-              <x-label for="addressLine1" :value="__('Address')" />
+              <x-label for="address" :value="__('Address')" />
+              <x-input class="block mt-1 w-full" value="{{ $organization->address }}" id="address"
+                placeholder="Address" type="text" name="address" />
               <div class="grid grid-cols-2 gap-4">
-                <div class="space-y-2">
-                  <x-input class="block mt-1 w-full" id="addressLine1" placeholder="Line 1" type="text"
-                    name="addressLine1" />
-                  <x-input class="block mt-1 w-full" id="city" placeholder="City" type="text" name="city" />
-                  <x-input class="block mt-1 w-full" id="zip" placeholder="Zip Code" type="text"
-                    name="zip" />
-                  <x-select class="block mt-1 w-full" :options="[['value' => 1, 'valueText' => 'Sri Lanka']]"></x-select>
+                <div class="mt-1">
+                  <x-input class="block mt-2 w-full" id="city" value="{{ $organization->city }}" placeholder="City"
+                    type="text" name="city" />
+                  <x-input class="block mt-2 w-full" id="zip" value="{{ $organization->zip }}"
+                    placeholder="Zip Code" type="text" name="zip" />
+                  <x-select class="block mt-2 w-full" :options="[['value' => 1, 'valueText' => 'Sri Lanka']]"></x-select>
                 </div>
-                <div class="space-y-2">
-                  <x-input class="block mt-1 w-full" id="addressLine2" placeholder="Line 2" type="text"
-                    name="addressLine2" />
-                  <x-input class="block mt-1 w-full" id="district" placeholder="District" type="text"
-                    name="district" />
-                  <x-input class="block mt-1 w-full" id="state" placeholder="State" type="text" name="state" />
+                <div class="mt-1">
+                  <x-input class="block mt-2 w-full" id="district" value="{{ $organization->district }}"
+                    placeholder="District" type="text" name="district" />
+                  <x-input class="block mt-2 w-full" id="state" value="{{ $organization->state }}"
+                    placeholder="State" type="text" name="state" />
                 </div>
               </div>
             </div>
             <div class="control mt-4 grid grid-cols-2 gap-4">
               <div>
-                <x-label for="phone" :value="__('Phone Number')" />
-                <x-input class="block mt-1 w-full" id="phone" placeholder="Organization Contact Number"
-                  type="tel" name="phone" />
+                <x-label for="phoneNumber" :value="__('Phone Number')" />
+                <x-input class="block mt-1 w-full" id="phoneNumber" value="{{ $organization->phoneNumber }}"
+                  placeholder="Organization Contact Number" type="tel" name="phoneNumber" />
               </div>
               <div>
                 <x-label for="email" :value="__('Email Address')"></x-label>
-                <x-input class="block mt-1 w-full" id="email" placeholder="Organization Email Address"
-                  type="email" name="email"></x-input>
+                <x-input class="block mt-1 w-full" id="email" value="{{ $organization->email }}"
+                  placeholder="Organization Email Address" type="email" name="email"></x-input>
               </div>
             </div>
             <div class="mt-4">
               <x-button>
-                {{ __('Create Organization') }}
+                @if ($mode == 'create')
+                  {{ __('Create Organization') }}
+                @endif
+                @if ($mode == 'edit')
+                  {{ __('Update Organization') }}
+                @endif
               </x-button>
             </div>
           </form>
