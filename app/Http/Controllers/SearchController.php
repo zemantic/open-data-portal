4b52040a9afee738a0617ebeed3ec83e2e203a5d
Rukshan <arkruka@gmail.com>
2022-11-24 07:16:21 +0530

    search controller initial commit

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Dataset;
+
+class SearchController extends Controller
+{
+    //
+    public function search(Request $request)
+    {
+        $validated = $request->validate([
+            "keyword" => "required",
+        ]);
+
+        $keyword = $request->keyword;
+        $datasets = Dataset::where("title", "LIKE", "%" . $keyword . "%")
+            ->orWhere("description", "LIKE", "%" . $keyword . "%")
+            ->get();
+
+        return view("searchResults", ["datasets" => $datasets]);
+    }
+}
