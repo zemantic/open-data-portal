7f2a0fb9fdb1e3fda297fdd48d62f79e3b5ccb06
Rukshan <arkruka@gmail.com>
2023-01-08 23:48:09 +0530

    refactor: :recycle: code format
    code format via prettier

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,16 +12,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create("users", function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('organization');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("name");
+            $table->string("email")->unique();
+            $table->string("organization")->nullable();
+            $table->timestamp("email_verified_at")->nullable();
+            $table->string("password");
             $table->rememberToken();
             $table->timestamps();
-            $table->boolean('retired')->default(false);
+            $table->boolean("retired")->default(false);
         });
     }
 
@@ -33,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists("users");
     }
 };
