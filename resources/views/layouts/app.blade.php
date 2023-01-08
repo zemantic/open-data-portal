98abff51b6e40a78709ff0f2fa130e61ab3ef8af
Rukshan <arkruka@gmail.com>
2023-01-09 00:00:00 +0530

    fix: :bug: changed navigation link
    changed navigation link from layouts to components to work with new location of the components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
 <body class="font-sans antialiased">
   <div class="min-h-screen bg-gray-100">
-    @include('layouts.navigation')
+    @include('components.navigation')
 
     <!-- Page Heading -->
     <header class="bg-white shadow">
